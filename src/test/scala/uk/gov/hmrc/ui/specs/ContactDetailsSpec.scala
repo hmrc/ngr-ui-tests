49e7a4866b359af0014a127279791fdaf1f04fe7
JakeReid2020 <75027964+JakeReid2020@users.noreply.github.com>
2025-02-26 13:12:30 +0000

    Seperating Contact Details from one login feature

--- src/test/scala/uk/gov/hmrc/ui/specs/ContactDetailsSpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/ContactDetailsSpec.scala
@@ -17,30 +17,41 @@
 package uk.gov.hmrc.ui.specs
 
 import uk.gov.hmrc.ui.pages.onelogin.{OlAuthenticationPages, OlSignInSelectorPage}
-import uk.gov.hmrc.ui.pages.{ConfirmContactDetailsPage, ContactNamePage, PhoneNumberPage, StartNowPage, StubPage}
-class RatePayerOLSignInSpec extends BaseSpec with StubPage {
+import uk.gov.hmrc.ui.pages._
+
+class ContactDetailsSpec extends BaseSpec with StubPage {
 
   private val StartNow         = StartNowPage
   private val OlSignInSelector = OlSignInSelectorPage
   private val OlAuthentication = OlAuthenticationPages
   private val env              = System.getProperty("environment")
 
-  Feature("Authenticate a ratepayer using OneLogin") {
-    Scenario("Authenticate a ratepayer using OneLogin") {
-
-      Given("Ratepayer on the Register for the business rates valuation service page")
+  Feature("Authenticated ratepayer using OneLogin Changes Contact Details") {
+    Scenario("Authenticate a ratepayer using OneLogin and change contact details") {
+      Given("Ratepayer logins through one login")
       StartNow.startNow()
-
-      And("One login option selected on selector page")
       OlSignInSelector.signInSelectorOL()
-
-      Then("Ratepayer successfully authenticated using One Login")
       if (env == "local" || env == "staging") {
         stubOlAuthentication()
       } else {
         OlAuthentication.olAuthentication("krutika.patil+11@digital.hmrc.gov.uk", "p2ssword1234")
       }
-
+      Then("Ratepayer is taken to the Confirm Contact Details page")
+      ConfirmContactDetailsPage.ConfirmContactDetails()
+      Then("Clicks the name link")
+      ConfirmContactDetailsPage.ClickNameLink()
+      Then("Name the page is shown")
+      ContactNamePage.ContactNameDetails()
+      Then("The ratepayer enters their name and clicks continue")
+      ContactNamePage.InputName()
+      Then("The ratepayer is taken to the Confirm Contact Details page")
+      ConfirmContactDetailsPage.ConfirmContactDetails()
+      Then("Clicks the name link add phone number link")
+      ConfirmContactDetailsPage.ClickAddPhoneNumberLink()
+      Then("The ratepayer is taken to the Phone Number Page")
+      PhoneNumberPage.PhoneNumberDetails()
+      Then("The ratepayer adds their number and clicks continue")
+      PhoneNumberPage.InputNumber()
     }
 
   }
