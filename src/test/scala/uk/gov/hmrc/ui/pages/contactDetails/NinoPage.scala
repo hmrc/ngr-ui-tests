a1b40b145c90acfde28f2cd3399447ed28de0f85
JakeReid2020 <75027964+JakeReid2020@users.noreply.github.com>
2025-03-17 11:59:30 +0000

    [NGR-659] Adding Nino Page

--- src/test/scala/uk/gov/hmrc/ui/pages/contactDetails/NinoPage.scala
+++ src/test/scala/uk/gov/hmrc/ui/pages/contactDetails/NinoPage.scala
@@ -1,5 +1,20 @@
 package uk.gov.hmrc.ui.pages.contactDetails
 
-class NinoPage {
+import org.openqa.selenium.By
+import uk.gov.hmrc.ui.pages.BasePage
+
+object NinoPage extends BasePage {
+
+  val ninoInput:By = By.id("nino-value")
+
+  def NinoDetails(): Unit = {
+    val text = "Provide your National Insurance number"
+    headerCheck(text)
+  }
+
+  def InputNino(): Unit = {
+    sendKeys(ninoInput, "")
+    click(continueButton)
+  }
 
 }
