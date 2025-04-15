20c42867c673458c5400a5f56fb54f06c01e77d0
KrutikaP <21079593+KrutikaP@users.noreply.github.com>
2025-04-15 15:38:32 +0100

    Added tests for optional address field

--- src/test/scala/uk/gov/hmrc/ui/pages/CheckYourAnswer.scala
+++ src/test/scala/uk/gov/hmrc/ui/pages/CheckYourAnswer.scala
@@ -17,7 +17,6 @@
 package uk.gov.hmrc.ui.pages
 
 import org.openqa.selenium.By
-import uk.gov.hmrc.ui.pages.contactDetails.ConfirmContactDetailsPage.getElementByXpath
 
 object CheckYourAnswer extends BasePage {
 
