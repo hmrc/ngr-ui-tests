9fe4df248ff0573e8bb29942ee131df354adaef6
WozCapGem <63247226+WozCapGem@users.noreply.github.com>
2025-06-06 16:24:40 +0100

    NGR-626, added singleSearchResults scenarios (#34)
    * NGR-626, added singleSearchResults scenarios
    
    * NGR-626 reformatting
    
    * Remove duplicate scenario
    
    * NGR-626 test update
    
    * NGR-626, test update
    
    * refactored tests
    
    ---------
    
    Co-authored-by: KrutikaP <21079593+KrutikaP@users.noreply.github.com>

--- src/test/scala/uk/gov/hmrc/ui/specs/AddAPropertySpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/AddAPropertySpec.scala
@@ -18,7 +18,7 @@ package uk.gov.hmrc.ui.specs
 
 import uk.gov.hmrc.ui.pages.contactDetails.{ConfirmContactDetailsPage, PhoneNumberPage}
 import uk.gov.hmrc.ui.pages.dashboard.DashboardHome
-import uk.gov.hmrc.ui.pages.propertyLinking.{AddAProperty, FindAProperty, WhatYouNeed}
+import uk.gov.hmrc.ui.pages.propertyLinking.{AddAProperty, FindAProperty, PropertySearchResultPage, WhatYouNeed}
 import uk.gov.hmrc.ui.pages.provideTRN.{ConfirmUTRPage, ProvideTRNPage}
 import uk.gov.hmrc.ui.pages.{CheckYourAnswer, RegisterComplete, StubPage}
 import uk.gov.hmrc.ui.utils.login.loginOl
@@ -89,25 +89,7 @@ class AddAPropertySpec extends BaseSpec with StubPage {
 
     }
 
-    Scenario("The user completes registration and navigates to the What you need page") {
-
-      Given("Ratepayer logins through one login")
-      loginOl()
-
-      Then("Ratepayer is now fully registered and is taken to the dashboard")
-      DashboardHome.DashboardHome(contactName)
-
-      Then("Ratepayer clicks the Add a Property link and is taken to the Add a Property page")
-      clickLink("Add a property")
-      AddAProperty.addAProperty()
-      click(continueButton)
-
-      Then("Ratepayer is taken to the What You Need page")
-      WhatYouNeed.whatYouNeed()
-//      contactLinkDisplay("contact your local council (opens in a new tab)")
-    }
-
-    Scenario("The user completes registration and navigates to the find a property page") {
+    Scenario("Testing for no Results Found for property search") {
 
       Given("Ratepayer logins through one login")
       loginOl()
@@ -128,6 +110,51 @@ class AddAPropertySpec extends BaseSpec with StubPage {
       FindAProperty.findProperty()
       FindAProperty.inputPostCode("LS1 9LB")
       FindAProperty.noResultsFound()
+    }
+
+    Scenario("The user completes registration and navigates to the find a property page and searches for a property") {
+
+      Given("Ratepayer logins through one login")
+      loginOl()
+
+      Then("Ratepayer is now fully registered and is taken to the dashboard")
+      DashboardHome.DashboardHome(contactName)
+
+      Then("Ratepayer clicks the Add a Property link and is taken to the Add a Property page")
+      clickLink("Add a property")
+      AddAProperty.addAProperty()
+      click(continueButton)
+
+      Then("Ratepayer is taken to the What You Need page")
+      WhatYouNeed.whatYouNeed()
+
+      /** ToDo Add tests for council naviagation */
+//     contactLinkDisplay("contact your local council (opens in a new tab)")
+      click(continueButton)
+
+      Then("Ratepayer is taken to the find a property page and searches for a property")
+      FindAProperty.findProperty()
+      FindAProperty.inputPostCode("BH1 7EY")
+
+      Then("Ratepayer is taken to the search results page")
+      PropertySearchResultPage.searchResult()
+
+      Then("Ratepayer clicks the search again link and is take back to the Find a property page")
+      clickLink("Search again")
+
+      Then("Ratepayer searches for a property")
+      FindAProperty.findProperty()
+      FindAProperty.inputPostCode("BH1 7EY")
+
+      Then("Ratepayer is taken to the search results page")
+      PropertySearchResultPage.searchResult()
+
+      Then("Ratepayer opens the help-if-you-cannot-find-your-property span")
+      PropertySearchResultPage.clickHelpSpan()
+
+      Then("Ratepayer clicks the search again link within the span and is take back to the Find a property page")
+      PropertySearchResultPage.searchAgainUnderHelpLink()
+      FindAProperty.findProperty()
     }
   }
 
