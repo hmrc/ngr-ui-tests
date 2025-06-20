6360700c941f9cb78791fb9342c5180f30a415ce
WozCapGem <63247226+WozCapGem@users.noreply.github.com>
2025-06-20 12:02:51 +0100

    NGR-640, tests broke WIP

--- src/test/scala/uk/gov/hmrc/ui/pages/propertyLinking/PropertyLinkingCYA.scala
+++ src/test/scala/uk/gov/hmrc/ui/pages/propertyLinking/PropertyLinkingCYA.scala
@@ -1,9 +1,28 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.ui.pages.propertyLinking
 
 import org.openqa.selenium.By
+import uk.gov.hmrc.configuration.TestEnvironment
 import uk.gov.hmrc.ui.pages.BasePage
 
 object PropertyLinkingCYA extends BasePage {
+
+  private val cya_url: String = TestEnvironment.url("ngr-property-linking-frontend") + "/check-your-answers"
 
   val changePropertyAddressLink = By.id("property-address")
   val changeRatepayerDate       = By.id("current-ratepayer")
@@ -12,23 +31,32 @@ object PropertyLinkingCYA extends BasePage {
 
   def clickChangePropertyAddressLink(): Unit =
     click(changePropertyAddressLink)
-  def clickChangeRatepayerDate(): Unit =
+  def clickChangeRatepayerDate(): Unit       =
     click(changeRatepayerDate)
-  def clickChangeBusinessRatesBill(): Unit =
+  def clickChangeBusinessRatesBill(): Unit   =
     click(changeBusinessRatesBill)
-  def clickChangeEvidenceDocument(): Unit =
+  def clickChangeEvidenceDocument(): Unit    =
     click(changeEvidenceDocument)
 
   def checkYourAnswer(): Unit =
     headerCheck("Check and confirm your details")
 
+  def hitCYAStep() =
+    getUrl(cya_url)
+
   def dateChangedCheck(date: String = "On or after 1 April 2026"): Unit = {
     val display = getElementByCssSelector("#when-did-you-become-the-current-ratepayer?-id")
     assert(date == display, "the date was not changed")
   }
-    def addressChangedCheck(address: String = "Bug me not pvt ltd, rodley lane, rodley, leeds, BH1 1HU"): Unit = {
-      val display = getElementByCssSelector("#property-to-add-to-account-id")
-      assert(address == display, "the address was not changed")
-    }
+
+  def billChangedCheck(yesNo: String): Unit = {
+    val display = getElementByCssSelector("#do-you-have-a-business-rates-bill-for-this-property?-id")
+    assert(yesNo == display, "the business rates bool was not changed")
+  }
+
+  def addressChangedCheck(address: String = "Bug me not pvt ltd, rodley lane, rodley, leeds, BH1 1HU"): Unit = {
+    val display = getElementByCssSelector("#property-to-add-to-account-id")
+    assert(address == display, "the address was not changed")
+  }
 
 }
--- src/test/scala/uk/gov/hmrc/ui/specs/PropertyLinkingCYASpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/PropertyLinkingCYASpec.scala
@@ -1,9 +1,29 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.ui.specs
 
 import org.openqa.selenium.By
-import uk.gov.hmrc.ui.pages.StubPage
+import uk.gov.hmrc.configuration.TestEnvironment
+import uk.gov.hmrc.ui.pages.contactDetails.{ConfirmContactDetailsPage, PhoneNumberPage}
+import uk.gov.hmrc.ui.pages.{CheckYourAnswer, RegisterComplete, StubPage}
 import uk.gov.hmrc.ui.pages.dashboard.DashboardHome
+import uk.gov.hmrc.ui.pages.propertyLinking.PropertyLinkingCYA.hitCYAStep
 import uk.gov.hmrc.ui.pages.propertyLinking._
+import uk.gov.hmrc.ui.pages.provideTRN.{ConfirmUTRPage, ProvideTRNPage}
 import uk.gov.hmrc.ui.utils.login.loginOl
 
 class PropertyLinkingCYASpec extends BaseSpec with StubPage {
@@ -17,6 +37,28 @@ class PropertyLinkingCYASpec extends BaseSpec with StubPage {
 
       Given("Ratepayer logins through one login")
       loginOl()
+
+//      Then("User provide phone number")
+//      PhoneNumberPage.userProvidesPhoneNumber()
+//
+//      Then("Ratepayer is taken to the Confirm Contact Details page")
+//      ConfirmContactDetailsPage.ConfirmContactDetails()
+//      click(continueButton)
+//
+//      Then("Ratepayer is taken to Provide TRN Page")
+//      ProvideTRNPage.provideYourTRN()
+//      click(continueButton)
+//
+//      Then("User selects 'Yes, I want to provide this UTR' and submit")
+//      ConfirmUTRPage.selectYes()
+//
+//      Then("The ratepayer is taken to the 'Check your answers' page")
+//      CheckYourAnswer.checkYourAnswer()
+//      click(continueButton)
+//
+//      Then("Ratepayer is taken to the Registration complete page")
+//      RegisterComplete.RegisterComplete()
+//      click(continueButton)
 
       Then("Ratepayer is now fully registered and is taken to the dashboard")
       DashboardHome.DashboardHome(contactName)
@@ -53,6 +95,7 @@ class PropertyLinkingCYASpec extends BaseSpec with StubPage {
       BusinessRateBillPage.selectYes()
 
       Then("The ratepayer hits the CYA page")
+      hitCYAStep()
       PropertyLinkingCYA.checkYourAnswer()
 
       Then("Clicks the change ratePayer link")
@@ -64,8 +107,11 @@ class PropertyLinkingCYASpec extends BaseSpec with StubPage {
       Then("Ratepayer selects 'After 1 April 2026' on 'When did you become the current ratepayer?' page")
       CurrentRatepayer.afterDateRadio()
 
+      Then("We wait")
+      Thread.sleep(15000)
+
       Then("The Ratepayer is taken back to the Check Your Answers page, with the ratepayer date changed")
-      PropertyLinkingCYA.checkYourAnswer()
+      // PropertyLinkingCYA.checkYourAnswer()
       PropertyLinkingCYA.dateChangedCheck()
     }
 
@@ -109,6 +155,7 @@ class PropertyLinkingCYASpec extends BaseSpec with StubPage {
       BusinessRateBillPage.selectYes()
 
       Then("The ratepayer hits the CYA page")
+      hitCYAStep()
       PropertyLinkingCYA.checkYourAnswer()
 
       Then("Clicks the change business rates bill link")
@@ -117,12 +164,15 @@ class PropertyLinkingCYASpec extends BaseSpec with StubPage {
       Then("The 'business rates bill for the property' page is shown")
       BusinessRateBillPage.BusinessRateBill()
 
-      Then("Ratepayer selects 'After 1 April 2026' on 'When did you become the current ratepayer?' page")
+      Then("Ratepayer selects 'no' on 'business rates bill for the property' page")
       BusinessRateBillPage.selectNo()
 
+      Then("We wait")
+      Thread.sleep(15000)
+
       Then("The Ratepayer is taken back to the Check Your Answers page, with the ratepayer date changed")
-      PropertyLinkingCYA.checkYourAnswer()
-      PropertyLinkingCYA.dateChangedCheck()
+      // PropertyLinkingCYA.checkYourAnswer()
+      PropertyLinkingCYA.billChangedCheck("No")
     }
 
     Scenario("Registered ratepayer goes through the flow to establish a property, and changes the property address") {
@@ -152,9 +202,15 @@ class PropertyLinkingCYASpec extends BaseSpec with StubPage {
       Then("Ratepayer click 'Select property' on the search results page")
       clickLink("Select property")
 
+      Then("We wait")
+      Thread.sleep(15000)
+
       Then("Ratepayer is taken to the selected property page, clicks the 'yes' radio and continues")
       SelectedProperty.selectedProperty()
       SelectedProperty.yesRadio()
+
+      Then("We wait")
+      Thread.sleep(15000)
 
       Then("Ratepayer selects 'Before 1 April 2026' on 'When did you become the current ratepayer?' page")
       CurrentRatepayer.currentRatepayer()
@@ -165,6 +221,7 @@ class PropertyLinkingCYASpec extends BaseSpec with StubPage {
       BusinessRateBillPage.selectYes()
 
       Then("The ratepayer hits the CYA page")
+      hitCYAStep()
       PropertyLinkingCYA.checkYourAnswer()
 
       Then("Clicks the change property address link")
@@ -178,8 +235,8 @@ class PropertyLinkingCYASpec extends BaseSpec with StubPage {
       PropertySearchResultPage.searchResult(postCode)
 
       Then("Ratepayer clicks the second 'Select property' on the search results page")
-       val link = By.xpath("//*[@id=\"main-content\"]/div/div/div[2]/div/table/tbody/tr[2]/td[5]/a")
-        waitForElementToBeClickable(link)
+      val link = By.xpath("//*[@id=\"main-content\"]/div/div/div[2]/div/table/tbody/tr[2]/td[5]/a")
+      waitForElementToBeClickable(link)
 
       Then("Ratepayer is taken to the selected property page, clicks the 'yes' radio and continues")
       SelectedProperty.selectedProperty()
@@ -194,11 +251,10 @@ class PropertyLinkingCYASpec extends BaseSpec with StubPage {
       BusinessRateBillPage.selectYes()
 
       Then("The Ratepayer is taken back to the Check Your Answers page, with the address changed")
+      hitCYAStep()
       PropertyLinkingCYA.checkYourAnswer()
       PropertyLinkingCYA.addressChangedCheck()
     }
-
-
 
   }
 }
