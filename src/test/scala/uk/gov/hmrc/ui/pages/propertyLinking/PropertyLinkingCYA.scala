9101851fd097bcb3702227b7e74a6c3313890510
WozCapGem <63247226+WozCapGem@users.noreply.github.com>
2025-06-20 15:16:22 +0100

    NGR-639, added tests work

---
+++ src/test/scala/uk/gov/hmrc/ui/pages/propertyLinking/PropertyLinkingCYA.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.pages.propertyLinking
+
+import org.openqa.selenium.By
+import uk.gov.hmrc.configuration.TestEnvironment
+import uk.gov.hmrc.ui.pages.BasePage
+
+object PropertyLinkingCYA extends BasePage {
+
+  private val cya_url: String = TestEnvironment.url("ngr-property-linking-frontend") + "/check-your-answers"
+
+  def hitCYAStep() =
+    getUrl(cya_url)
+
+  val changePropertyAddressLink = By.id("property-address")
+  val changeRatepayerDate       = By.id("current-ratepayer")
+  val changeBusinessRatesBill   = By.id("business-rates-bill")
+  val changeEvidenceDocument    = By.id("evidence-document")
+
+  def clickChangePropertyAddressLink(): Unit =
+    click(changePropertyAddressLink)
+
+  def clickChangeRatepayerDate(): Unit =
+    click(changeRatepayerDate)
+
+  def clickChangeBusinessRatesBill(): Unit =
+    click(changeBusinessRatesBill)
+
+  def clickChangeEvidenceDocument(): Unit =
+    click(changeEvidenceDocument)
+
+  def checkYourAnswer(): Unit =
+    headerCheck("Check and confirm your details")
+
+}
