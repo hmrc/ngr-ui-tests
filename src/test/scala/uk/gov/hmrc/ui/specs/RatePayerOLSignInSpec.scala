a288a3e2b20cbc364622b2fbb340fd8e4506209c
WozCapGem <63247226+WozCapGem@users.noreply.github.com>
2025-07-07 08:39:48 +0100

    NGR-upscan-test, uncommented other tests

--- src/test/scala/uk/gov/hmrc/ui/specs/RatePayerOLSignInSpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/RatePayerOLSignInSpec.scala
@@ -14,63 +14,47 @@
  * limitations under the License.
  */
 
-///*
-// * Copyright 2025 HM Revenue & Customs
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// *
-// *     http://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
-//
-//package uk.gov.hmrc.ui.specs
-//
-//import uk.gov.hmrc.ui.pages.SignIn.{OLAuthenticationPages, SignInSelectorPage}
-//import uk.gov.hmrc.ui.pages.contactDetails.ConfirmContactDetailsPage.ConfirmContactDetails
-//import uk.gov.hmrc.ui.pages.contactDetails.PhoneNumberPage
-//import uk.gov.hmrc.ui.pages.{StartNowPage, StubPage}
-//import uk.gov.hmrc.ui.utils.mongo.RegistrationDB
-//class RatePayerOLSignInSpec extends BaseSpec with StubPage {
-//
-//  private val StartNow         = StartNowPage
-//  private val OlSignInSelector = SignInSelectorPage
-//  private val OlAuthentication = OLAuthenticationPages
-//  private val env              = System.getProperty("environment")
-//
-//  Feature("Authenticate a ratepayer using OneLogin") {
-//    RegistrationDB.cleanup()
-//    Scenario("Authenticate a ratepayer using OneLogin") {
-//
-//      Given("Ratepayer obtains the centralised-auth private beta cookies")
-//      OLAuthenticationPages.betaPageStep()
-//
-//      Given("Ratepayer on the Register for the business rates valuation service page")
-//      StartNow.startNow()
-//
-//      When("One login option selected on selector page")
-//      OlSignInSelector.signInSelectorOL()
-//
-//      And("Ratepayer authenticate using One Login")
-//      if (env == "local" || env == "staging") {
-//        stubOlAuthentication()
-//      } else {
-//        OlAuthentication.olAuthentication("krutika.patil+11@digital.hmrc.gov.uk", "p2ssword1234")
-//      }
-//
-//      Then("User provide phone number")
-//      PhoneNumberPage.userProvidesPhoneNumber()
-//
-//      Then("Ratepayer successfully authenticated navigated to contact details page ")
-//      ConfirmContactDetails()
-//    }
-//
-//  }
-//
-//}
+package uk.gov.hmrc.ui.specs
+
+import uk.gov.hmrc.ui.pages.SignIn.{OLAuthenticationPages, SignInSelectorPage}
+import uk.gov.hmrc.ui.pages.contactDetails.ConfirmContactDetailsPage.ConfirmContactDetails
+import uk.gov.hmrc.ui.pages.contactDetails.PhoneNumberPage
+import uk.gov.hmrc.ui.pages.{StartNowPage, StubPage}
+import uk.gov.hmrc.ui.utils.mongo.RegistrationDB
+class RatePayerOLSignInSpec extends BaseSpec with StubPage {
+
+  private val StartNow         = StartNowPage
+  private val OlSignInSelector = SignInSelectorPage
+  private val OlAuthentication = OLAuthenticationPages
+  private val env              = System.getProperty("environment")
+
+  Feature("Authenticate a ratepayer using OneLogin") {
+    RegistrationDB.cleanup()
+    Scenario("Authenticate a ratepayer using OneLogin") {
+
+      Given("Ratepayer obtains the centralised-auth private beta cookies")
+      OLAuthenticationPages.betaPageStep()
+
+      Given("Ratepayer on the Register for the business rates valuation service page")
+      StartNow.startNow()
+
+      When("One login option selected on selector page")
+      OlSignInSelector.signInSelectorOL()
+
+      And("Ratepayer authenticate using One Login")
+      if (env == "local" || env == "staging") {
+        stubOlAuthentication()
+      } else {
+        OlAuthentication.olAuthentication("krutika.patil+11@digital.hmrc.gov.uk", "p2ssword1234")
+      }
+
+      Then("User provide phone number")
+      PhoneNumberPage.userProvidesPhoneNumber()
+
+      Then("Ratepayer successfully authenticated navigated to contact details page ")
+      ConfirmContactDetails()
+    }
+
+  }
+
+}
--- src/test/scala/uk/gov/hmrc/ui/specs/SignOutSpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/SignOutSpec.scala
@@ -14,70 +14,54 @@
  * limitations under the License.
  */
 
-///*
-// * Copyright 2025 HM Revenue & Customs
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// *
-// *     http://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
-//
-//package uk.gov.hmrc.ui.specs
-//
-//import uk.gov.hmrc.ui.pages.FeedbackPage
-//import uk.gov.hmrc.ui.pages.SignIn.SignInSelectorPage
-//import uk.gov.hmrc.ui.pages.{SignOutPage, StubPage}
-//import uk.gov.hmrc.ui.utils.login.loginOl
-//
-//class SignOutSpec extends BaseSpec with StubPage {
-//
-//  Feature("Testing the Sign out functionality") {
-//
-//    Scenario("Testing the Sign out functionality") {
-//      Given("Ratepayer logins through one login")
-//      loginOl()
-//
-//      And("The user click on Sign out link")
-//      clickLink("Sign out")
-//
-//      Then("The user navigated to 'You have signed out' page")
-//      SignOutPage.signOut()
-//    }
-//
-//    Scenario("Testing 'Sign in' link from Signed out page") {
-//      Given("Ratepayer logins through one login")
-//      loginOl()
-//
-//      And("The user navigated to 'You have signed out' page after clicking on Sign out link")
-//      clickLink("Sign out")
-//      SignOutPage.signOut()
-//
-//      When("the user click on 'Sign in to the service' link")
-//      clickLink("Sign in to the service")
-//
-//      Then("The user navigated to 'sign in to HMRC' page")
-//      SignInSelectorPage.signInSelector()
-//    }
-//
-//    Scenario("Testing 'short survey' link from Signed out page") {
-//
-//      Given("The ratepayer logins through one login")
-//      loginOl()
-//
-//      And("The ratepayer Sign out and click on survey link")
-//      clickLink("Sign out")
-//      clickLink("Take a short survey")
-//
-//      Then("The ratepayer navigated to give feedback page")
-//      FeedbackPage.giveFeedbackPage()
-//    }
-//  }
-//}
+package uk.gov.hmrc.ui.specs
+
+import uk.gov.hmrc.ui.pages.FeedbackPage
+import uk.gov.hmrc.ui.pages.SignIn.SignInSelectorPage
+import uk.gov.hmrc.ui.pages.{SignOutPage, StubPage}
+import uk.gov.hmrc.ui.utils.login.loginOl
+
+class SignOutSpec extends BaseSpec with StubPage {
+
+  Feature("Testing the Sign out functionality") {
+
+    Scenario("Testing the Sign out functionality") {
+      Given("Ratepayer logins through one login")
+      loginOl()
+
+      And("The user click on Sign out link")
+      clickLink("Sign out")
+
+      Then("The user navigated to 'You have signed out' page")
+      SignOutPage.signOut()
+    }
+
+    Scenario("Testing 'Sign in' link from Signed out page") {
+      Given("Ratepayer logins through one login")
+      loginOl()
+
+      And("The user navigated to 'You have signed out' page after clicking on Sign out link")
+      clickLink("Sign out")
+      SignOutPage.signOut()
+
+      When("the user click on 'Sign in to the service' link")
+      clickLink("Sign in to the service")
+
+      Then("The user navigated to 'sign in to HMRC' page")
+      SignInSelectorPage.signInSelector()
+    }
+
+    Scenario("Testing 'short survey' link from Signed out page") {
+
+      Given("The ratepayer logins through one login")
+      loginOl()
+
+      And("The ratepayer Sign out and click on survey link")
+      clickLink("Sign out")
+      clickLink("Take a short survey")
+
+      Then("The ratepayer navigated to give feedback page")
+      FeedbackPage.giveFeedbackPage()
+    }
+  }
+}
