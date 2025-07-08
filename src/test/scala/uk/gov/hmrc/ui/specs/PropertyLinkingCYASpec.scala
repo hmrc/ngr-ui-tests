50d4d436fc19490a3e7a615e2f90166b78aabba7
WozCapGem <63247226+WozCapGem@users.noreply.github.com>
2025-07-08 12:18:08 +0100

    pr builder update 1

--- src/test/scala/uk/gov/hmrc/ui/specs/PropertyLinkingCYASpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/PropertyLinkingCYASpec.scala
@@ -150,9 +150,7 @@ class PropertyLinkingCYASpec extends BaseSpec with StubPage {
       PropertyLinkingCYA.billChangedCheck("No")
     }
 
-    Scenario(
-      "Registered ratepayer goes through the flow to establish a property, and changes the rates bill uploaded evidence"
-    ) {
+    Scenario("Registered ratepayer goes through the flow to establish a property, and changes the rates bill uploaded evidence") {
 
       Given("Ratepayer logins through one login")
       loginOl()
@@ -265,6 +263,5 @@ class PropertyLinkingCYASpec extends BaseSpec with StubPage {
       PropertyLinkingCYA.checkYourAnswer()
       PropertyLinkingCYA.connectionChangedCheck("Occupier")
     }
-
   }
 }
