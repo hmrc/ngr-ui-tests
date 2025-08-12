38ede3abd0b70af078e1fa004df9aee572bbcda8
KrutikaP <21079593+KrutikaP@users.noreply.github.com>
2025-08-12 10:28:31 +0100

    formatted

--- src/test/scala/uk/gov/hmrc/ui/specs/PropertyLinking/AddAPropertySpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/PropertyLinking/AddAPropertySpec.scala
@@ -95,7 +95,7 @@ class AddAPropertySpec extends BaseSpec with StubPage {
 
       /** Tests for NO radio button selection * */
       And("The ratepayers selects 'No' on 'business rates bill for the property' page")
-      BusinessRateBillPage.BusinessRateBill()
+      BusinessRateBillPage.businessRateBill()
       BusinessRateBillPage.selectNo()
 
       Then("The ratepayer selects 'lease' on 'What evidence can you provide?' page")
