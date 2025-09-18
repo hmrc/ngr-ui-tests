141a695c2930d017f900e0ad6662e0ecbb7ff65f
Jake Reid <75027964+JakeReid2020@users.noreply.github.com>
2025-09-18 10:00:51 +0100

    [NGR-1126] Adding InterimRentSetByTheCourtPage (#87)
    * [NGR-1126] Adding InterimRentSetByTheCourtPage
    
    * [NGR-1126] Fixes
    
    * [NGR-1126] Correcting title

---
+++ src/test/scala/uk/gov/hmrc/ui/pages/RALD/InterimRentSetByTheCourtPage.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.pages.RALD
+
+import org.openqa.selenium.By
+import uk.gov.hmrc.ui.pages.BasePage
+import uk.gov.hmrc.ui.pages.RALD.RentDatesPage.{headerCheck, sendKeys}
+
+object InterimRentSetByTheCourtPage extends BasePage {
+
+  def interimRentSetByTheCourtPage(): Unit =
+    headerCheck("Interim rent set by the court")
+
+  def interimHowMuchInput(amount: String): Unit =
+    sendKeys(By.id("interimAmount"), amount)
+
+  def interimRentSetByTheCourtDateInput(month: String, year: String): Unit =
+    dateInput(month, year, "date")
+
+  private def dateInput(month: String, year: String, whichDate: String): Unit = {
+    sendKeys(By.id(s"$whichDate.month"), month)
+    sendKeys(By.id(s"$whichDate.year"), year)
+  }
+}
