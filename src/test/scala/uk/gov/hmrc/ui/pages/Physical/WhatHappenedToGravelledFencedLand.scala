09a55ce00f6319307f5643297b97ee47ddfbe0d0
ZertekesDigital1 <224573701+ZertekesDigital1@users.noreply.github.com>
2025-09-19 12:57:46 +0100

    adding_screens

--- src/test/scala/uk/gov/hmrc/ui/pages/Physical/WhatHappenedToGravelledFencedLand.scala
+++ src/test/scala/uk/gov/hmrc/ui/pages/Physical/WhatHappenedToGravelledFencedLand.scala
@@ -1,16 +1,32 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.ui.pages.Physical
 
 import uk.gov.hmrc.ui.pages.BasePage
 
-object WhatHappenedToGravelledFencedLand extends BasePage{
+object WhatHappenedToGravelledFencedLand extends BasePage {
   def whatHappenedToGravelledFencedLandHeader(): Unit =
     headerCheck("What has happened to gravelled, fenced land?")
 
   def whatHappenedToGravelledFencedLandRadio(gravelledFencedLand: String): Unit = {
     val radioCheckId = gravelledFencedLand match {
-      case "You added gravelled, fenced land"          => "value_0"
-      case "You removed some gravelled, fenced land"         => "value_1"
-      case "You removed all gravelled, fenced land" => "value_2"
+      case "You added gravelled, fenced land"        => "value_0"
+      case "You removed some gravelled, fenced land" => "value_1"
+      case "You removed all gravelled, fenced land"  => "value_2"
     }
     click(getElementById(radioCheckId))
   }
--- src/test/scala/uk/gov/hmrc/ui/pages/Physical/WhatHappenedToOutdoorSeating.scala
+++ src/test/scala/uk/gov/hmrc/ui/pages/Physical/WhatHappenedToOutdoorSeating.scala
@@ -1,17 +1,33 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.ui.pages.Physical
 
 import uk.gov.hmrc.ui.pages.BasePage
 
-object WhatHappenedToOutdoorSeating extends BasePage{
+object WhatHappenedToOutdoorSeating extends BasePage {
 
   def whatHappenedToOutdoorSeatingHeader(): Unit =
     headerCheck("What has happened to outdoor seating?")
 
   def whatHappenedToOutdoorSeatingRadio(outdoorSeating: String): Unit = {
     val radioCheckId = outdoorSeating match {
-      case "You added outdoor seating"          => "value_0"
-      case "You removed some outdoor seating"         => "value_1"
-      case "You removed all outdoor seating" => "value_2"
+      case "You added outdoor seating"        => "value_0"
+      case "You removed some outdoor seating" => "value_1"
+      case "You removed all outdoor seating"  => "value_2"
     }
     click(getElementById(radioCheckId))
   }
