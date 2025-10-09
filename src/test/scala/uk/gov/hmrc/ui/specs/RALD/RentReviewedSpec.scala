2cf65bcfb7b42714045628ba614e5d24586717ee
anna-shen-hmrc <anna-shen-hmrc@users.noreply.github.com>
2025-10-09 14:52:49 +0100

    NGR-1130: adding ATs when rent based on TOC

--- src/test/scala/uk/gov/hmrc/ui/specs/RALD/RentReviewedSpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/RALD/RentReviewedSpec.scala
@@ -43,7 +43,7 @@ class RentReviewedSpec extends BaseSpec with StubPage {
       TellUsAboutYourRent.tellUsAboutYourRent()
       continueButtonClick()
 
-      /* relationship With The Landlord = 'Company pension fund'*/
+      /* Has relationship With The Landlord*/
       Then("The user inputs the landlords name and selects family member as type")
       Landlord.landlord()
       Landlord.landlordNameInput(landlordName = "Bob")
@@ -51,20 +51,109 @@ class RentReviewedSpec extends BaseSpec with StubPage {
       Landlord.supplyRelationship("I am the tenant")
       continueButtonClick()
 
-      Then("The user selects verbal agreement as there agreement type")
-      WhatTypeOfAgreement.TypeOfAgreement()
-      WhatTypeOfAgreement.verbalRadio()
+      /*'What is your rent based on?' = 'Indexation'*/
+      Then("The user selects other and input reason on 'What is your rent based on?' page and submit")
+      WhatIsRentBasedOn.whatIsRentBasedOn()
+      WhatIsRentBasedOn.selectRentBaseOn("Indexation")
       continueButtonClick()
 
-      Then("The user input agreement start date and selected yes for open-ended")
-      AgreementVerbal.agreementVerbal()
-      AgreementVerbal.startDateInput("23", "4", "2025")
-      AgreementVerbal.yesOpenEndedAgreementRadio()
+      Then("The user selects what their rent includes")
+      WhatYourRentIncludesPage.whatYourRentIncludes()
+      WhatYourRentIncludesPage.livingAccommodationRadio("No")
+      WhatYourRentIncludesPage.rentPartAddressRadio("Yes")
+      WhatYourRentIncludesPage.rentEmptyShellRadio("Yes")
+      WhatYourRentIncludesPage.rentIncBusinessRatesRadio("Yes")
+      WhatYourRentIncludesPage.rentIncWaterChargesRadio("Yes")
+      WhatYourRentIncludesPage.rentIncServiceRadio("Yes")
       continueButtonClick()
 
-      Then("The user enter how much is total annual rent")
-      HowMuchIsTotalAnnualRent.howMuchIsTotalAnnualRent()
-      HowMuchIsTotalAnnualRent.inputTotalAnnualRent("3500")
+      Then("The user selects 'Yes' to having parking spaces or garages")
+      DoesYourRentIncludeParkingPage.doesYourRentIncludeParking()
+      DoesYourRentIncludeParkingPage.yesRadio()
+      continueButtonClick()
+
+      Then("The user enters uncovered, covered spaces and garages")
+      HowManyParkingSpacesOrGaragesIncludedInRentPage.howManyParkingSpacesOrGaragesIncludedInRent()
+      HowManyParkingSpacesOrGaragesIncludedInRentPage.enterUncoveredSpaces("2")
+      HowManyParkingSpacesOrGaragesIncludedInRentPage.enterCoveredSpaces("10")
+      HowManyParkingSpacesOrGaragesIncludedInRentPage.enterGarages("5")
+      continueButtonClick()
+
+      Then("The user select no to pay extra parking spaces not included in rent")
+      DoYouPayExtraForParkingSpaces.doYouPayExtraForParkingSpaces()
+      DoYouPayExtraForParkingSpaces.selectPayExtraRadio("no")
+      continueButtonClick()
+
+      Then("The user selects you to who pays for internal, external and building insurance repairs")
+      RepairsAndInsurancePage.repairsAndInsurance()
+      RepairsAndInsurancePage.whoPaysForInternalRepairsRadio("The landlord")
+      RepairsAndInsurancePage.whoPaysForExternalRepairsRadio("The landlord")
+      RepairsAndInsurancePage.WhoPaysForBuildingInsuranceRepairs("The landlord")
+      continueButtonClick()
+    }
+
+    Scenario("The user reviewed their rent, rent based on: Total Occupancy Cost leases (TOCs)") {
+      Given("Ratepayer logins through one login")
+      loginOl()
+
+      When("The ratepayer clicks on 'Tell us about a change' link from dashboard")
+      dashboard()
+      clickLink("Tell us about a change")
+
+      Then("The ratepayers selects the property and proceed through the new agreement journey")
+      YourProperty.yourProperty()
+      clickLink("Select property")
+
+      Then("The user selects renewed review your rent link to review their rent")
+      WhatDoYouWantToTellUs.whatDoYouWantToTellUs()
+      clickLink("You reviewed your rent")
+      TellUsAboutYourRent.tellUsAboutYourRent()
+      continueButtonClick()
+
+      /* Has relationship With The Landlord*/
+      Then("The user inputs the landlords name and selects family member as type")
+      Landlord.landlord()
+      Landlord.landlordNameInput(landlordName = "Bob")
+      Landlord.yesRadio()
+      Landlord.supplyRelationship("I am the tenant")
+      continueButtonClick()
+
+      /*'What is your rent based on?' = 'Total Occupancy Cost leases (TOCs)'*/
+      Then("The user selects other and input reason on 'What is your rent based on?' page and submit")
+      WhatIsRentBasedOn.whatIsRentBasedOn()
+      WhatIsRentBasedOn.selectRentBaseOn("Total Occupancy Cost leases (TOCs)")
+      continueButtonClick()
+
+      Then("The user selects what their rent includes")
+      WhatYourRentIncludesPage.whatYourRentIncludes()
+      WhatYourRentIncludesPage.livingAccommodationRadio("No")
+      WhatYourRentIncludesPage.rentPartAddressRadio("Yes")
+      WhatYourRentIncludesPage.rentEmptyShellRadio("Yes")
+      continueButtonClick()
+
+      Then("The user selects 'No' to having parking spaces or garages")
+      DoesYourRentIncludeParkingPage.doesYourRentIncludeParking()
+      DoesYourRentIncludeParkingPage.noRadio()
+      continueButtonClick()
+
+      Then("The user select yes to pay extra parking spaces not included in rent")
+      DoYouPayExtraForParkingSpaces.doYouPayExtraForParkingSpaces()
+      DoYouPayExtraForParkingSpaces.selectPayExtraRadio("yes")
+      continueButtonClick()
+
+      Then(
+        "The user enters 3 for uncovered parking spaces, 4000 for how much extra they pay with an agreement date of 19-01-2020"
+      )
+      ParkingSpacesOrGaragesNotIncludedInYourRentPage.enterUncoveredSpaces("3")
+      ParkingSpacesOrGaragesNotIncludedInYourRentPage.totalCost("4000")
+      ParkingSpacesOrGaragesNotIncludedInYourRentPage.agreementDateInput(day = "19", month = "01", year = "1997")
+      continueButtonClick()
+
+      Then("The user selects you to who pays for internal, external and building insurance repairs")
+      RepairsAndInsurancePage.repairsAndInsurance()
+      RepairsAndInsurancePage.whoPaysForInternalRepairsRadio("The landlord")
+      RepairsAndInsurancePage.whoPaysForExternalRepairsRadio("The landlord")
+      RepairsAndInsurancePage.WhoPaysForBuildingInsuranceRepairs("The landlord")
       continueButtonClick()
     }
   }
