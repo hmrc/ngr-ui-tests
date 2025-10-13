325f958b765eefecb6b24e0b4799160c73266740
Ashwini Rudrappa <11343826+ashwinirudrappa@users.noreply.github.com>
2025-10-13 13:55:36 +0100

    NGR-2536: Add IV login steps

--- src/test/scala/uk/gov/hmrc/ui/pages/StubPage.scala
+++ src/test/scala/uk/gov/hmrc/ui/pages/StubPage.scala
@@ -52,70 +52,11 @@ trait StubPage extends BasePage {
     click(submit)
   }
 
-  def IvNinoStub(): Unit = {
-    headerCheck("Test Only Nino access")
-    click(secondarySubmit)
-  }
-
-  def linkToHMRCRecordStub(): Unit = {
-    headerCheck("We need to link your GOV.UK One Login to your HMRC records")
-    click(answer)
-    click(submit)
-  }
-
-  def enterNinoStub(): Unit = {
-    headerCheck("Enter your National Insurance number")
-    sendKeys(nino, "AA000003D")
-    click(submit)
-  }
-
-  def checkNino(): Unit = {
-    headerCheck("Check your National Insurance number")
-    click(answer)
-    click(submit)
-  }
-
-  def provideTaxReference(): Unit = {
-    headerCheck("Provide your tax reference number")
-    click(continue)
-  }
-
-  def confirmSAReference(): Unit = {
-    headerCheck("Confirm your Self Assessment Unique Taxpayer Reference")
-    click(confirmUTR)
-    click(continue)
-  }
-
-  def provideNino(): Unit = {
-    headerCheck("Provide your National Insurance number")
-    sendKeys(ninoValue, "AA000003D")
-    click(continue)
-  }
-
-  def checkYourAnswer(): Unit = {
-    headerCheck("Check your answers")
-    click(continue)
-  }
-
-  def complete(): Unit               = {
-    headerCheck("GOV.UK One Login set up complete")
-    click(submit)
-  }
-  def registrationSuccessful(): Unit = {
-    headerCheck("Registration Successful")
-    click(continue)
-  }
-
   def stubOlAuthentication(): Unit = {
     loginStub()
     // This page will be added in the future time.
 //    proveYourIdentity()
     IvStub()
-    IvNinoStub()
-    linkToHMRCRecordStub()
-    enterNinoStub()
-    checkNino()
-    complete()
   }
 
   def stubGgAuthentication(): Unit = {
