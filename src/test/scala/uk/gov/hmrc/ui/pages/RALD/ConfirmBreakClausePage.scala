151c44015146a01618a744bfa698114f6f9a346c
Pete Robinson <77154273+peterdrobinson@users.noreply.github.com>
2025-10-17 12:14:09 +0100

    NGR-2538 - Added AT tests for missing pages

---
+++ src/test/scala/uk/gov/hmrc/ui/pages/RALD/ConfirmBreakClausePage.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.pages.RALD
+
+import org.openqa.selenium.By
+import uk.gov.hmrc.ui.pages.BasePage
+
+object ConfirmBreakClausePage extends BasePage {
+
+  val confirmBreakClauseID: By = By.id("confirmBreakClause-radio-value")
+
+  def confirmBreakClause(): Unit =
+    headerCheck("confirmBreakClause")
+
+  def confirmBreakClauseInput(confirmBreakClause: String): Unit =
+    sendKeys(confirmBreakClauseID, confirmBreakClause)
+
+  def yesRadio(): Unit =
+    click(getElementById("confirmBreakClause-radio-value"))
+
+  def noRadio(): Unit =
+    click(getElementById("confirmBreakClause-radio-value-2"))
+
+}
