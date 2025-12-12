695a07559a05663c86fddce48ddb2a646e2e4d16
Ashwini Rudrappa <11343826+ashwinirudrappa@users.noreply.github.com>
2025-12-12 10:31:51 +0000

    NGR-3767:update ui-test-runner

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"            %% "ui-test-runner"     % "0.50.0"   % Test,
+    "uk.gov.hmrc"            %% "ui-test-runner"     % "0.52.0"   % Test,
     "org.scalatest"          %% "scalatest"          % "3.2.19"   % Test,
     "ch.qos.logback"          % "logback-classic"    % "1.5.22"   % Test,
     "io.cucumber"             % "cucumber-junit"     % "7.33.0"   % Test,
